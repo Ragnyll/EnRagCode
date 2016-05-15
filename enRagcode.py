c3792a4108565db40b635cce31f4eb4b58156bf1
ragnyll <justanotherjakeyg@gmail.com>
2016-05-15 12:23:56 -0500

    fixed this program so its not a redundant piece of crap

--- enRagcode.py
+++ enRagcode.py
@@ -2,63 +2,63 @@ import string
 import random
 import json
 
-def create_encryption_key():
+def create_encoding_key():
     crypt_key = {}
     valid_chars = list(string.printable)
     exchangeable_chars = list(string.printable)
     while len(valid_chars) > 0:
         crypt_key[valid_chars.pop(0)] = exchangeable_chars.pop(random.randrange(0,
             len(exchangeable_chars), 1))
+
     return crypt_key
 
-def create_decryption_key(encrypted_dict):
-    decrypt_key = {}
-    #swap the keys and values to create the decrypt_key
-    for key, val in encrypted_dict.items():
-        decrypt_key[val] = key
-    return decrypt_key
+def create_decoding_key(encodeed_dict):
+    decode_key = {}
+    #swap the keys and values to create the decode_key
+    for key, val in encodeed_dict.items():
+        decode_key[val] = key
 
-def encrypt(encryption_key, estring):
-    encrypted_string = ''
+    return decode_key
+
+def encode(encoding_key, estring):
+    encodeed_string = ''
     for c in estring:
-        encrypted_string += encryption_key[c]
-    return encrypted_string
+        encodeed_string += encoding_key[c]
 
-def decrypt(decrypt_key, encrypted_string):
-    decrypted_string = ''
-    for c in encrypted_string:
-        decrypted_string += decrypt_key[c]
-    return decrypted_string
+    return encodeed_string
 
-# give it the encrypted dict
-def export_encryption_key(e1):
-    e_key = open('encryption_key.json', 'w')
+def decode(decode_key, encodeed_string):
+    decodeed_string = ''
+    for c in encodeed_string:
+        decodeed_string += decode_key[c]
+
+    return decodeed_string
+
+
+def export_encoding_key(e1):
+    e_key = open('encoding_key.json', 'w')
     json.dump(e1, e_key)
     e_key.close()
 
-# give it the decrypt dict
-def export_decryption_key(d1):
-    d_key = open('decryption_key.json', 'w')
+def export_decoding_key(d1):
+    d_key = open('decoding_key.json', 'w')
     json.dump(d1, d_key)
-    des_d_1.close()
+    d_key.close()
 
-# add error handling if there is no encryption key files
+# add error handling if there is no encoding key files
 # add .close for files
 # might be worth removing the hardcode of file names
-def import_encryption_keys():
-    e1 = json.load(open('des_e_1.json')) # how does one close this?
-    e2 = json.load(open('des_e_2.json'))
-    e3 = json.load(open('des_e_3.json'))
-    return e1, e2, e3
+def import_encoding_key():
+    e1 = json.load(open('encoding_key.json')) # how does one close this?
+    return e1
 
-def import_decryption_keys():
-    d1 = json.load(open('des_d_1.json')) # how does one close this?
-    d2 = json.load(open('des_d_2.json'))
-    d3 = json.load(open('des_d_3.json'))
-    return d1, d2, d3
+def import_decoding_key():
+    d1 = json.load(open('decoding_key.json')) # how does one close this?
+    return d1
+
 # currently only tested Text I/O
-def encrypt_file(use_keys):
-    file_path = input("Please give a path to a file to encrypt: ")
+def encode_file(use_keys):
+    file_path = input("Please give a path to a file to encode: ")
     f = open(file_path, 'r+')
     if not f.writable():
         print('Error: File is not writable! Terminating Program.')
@@ -66,34 +66,31 @@ def encrypt_file(use_keys):
     data  = f.read()
 
     if use_keys:
-        e1, e2, e3 = import_encryption_keys()
+        e1 = import_encoding_key()
     else:
-        e1 = create_encryption_key()
-        e2 = create_encryption_key()
-        e3 = create_encryption_key()
-        export_des3_encryption_keys(e1, e2, e3)
-        export_des3_decryption_keys(create_decryption_key(e1), create_decryption_key(e2),
-            create_decryption_key(e3))
+        e1 = create_encoding_key()
+        export_encoding_key(e1)
+        export_decoding_key(create_decoding_key(e1))
 
-    data = des3_encrypt(e1, e2, e3, data)
+    data = encode(e1, data)
     f.seek(0)
     f.write(data)
     f.truncate()
     f.close()
-    print("File Encrypted!")
-    # currently need the encryption keys to returned for debuggging
-    return e1, e2, e3
+    print("File encodeed!")
+    # currently need the encoding keys to returned for debuggging
+    return e1
 
-def decrypt_file():
-    file_path = input("Please give a path to the file to decrypt: ")
+def decode_file():
+    file_path = input("Please give a path to the file to decode: ")
     f = open(file_path, 'r+')
     if not f.writable():
         print('Error: File is not writable! Terminating Program.')
         exit()
     data = f.read()
     # this will be removed when the read from decrypion file function is done
-    d1, d2, d3 = import_decryption_keys()
-    data = des3_decrypt(d1, d2, d3, data)
+    d1 = import_decoding_key()
+    data = decode(d1, data)
     f.seek(0)
     f.write(data)
     f.truncate()
@@ -104,24 +101,24 @@ def main():
     alive = True
     while(alive):
         print("Please Select:")
-        print("1.Encrypt a message")
-        print("2.Decrypt a message")
+        print("1.encode a message")
+        print("2.decode a message")
         print("4.Exit")
         option = str(input(""))
         if option == '1':
-            print("Encrypt mode selected\n")
-            print("1. Use my own encryption keys")
-            print("2. Generate an Encryption/Decryption key set")
-            encrypt_or_decrypt = str(input(""))
-            if encrypt_or_decrypt == '1':
-                encrypt_file(True)
-            elif encrypt_or_decrypt == '2':
-                encrypt_file(False)
+            print("encode mode selected\n")
+            print("1. Use my own encoding keys")
+            print("2. Generate an encoding/decoding key set")
+            encode_or_decode = str(input(""))
+            if encode_or_decode == '1':
+                encode_file(True)
+            elif encode_or_decode == '2':
+                encode_file(False)
             else:
                 print("Invalid selection. Redirecting to root menu.")
         elif option == '2':
-            print("Decryption mode selected")
-            decrypt_file()
+            print("decoding mode selected")
+            decode_file()
         elif option == '4':
             print("Terminating Program")
             alive = False
